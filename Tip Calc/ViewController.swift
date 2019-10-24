4a2276fec5e5c7fc1cdaba4b304347955c2178ab
brickatyourfeet <brickatyourfeet@gmail.com>
2019-10-23 21:56:21 -0700

    Initial Commit

---
+++ Tip Calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calc
+//
+//  Created by Kyle Braden on 10/23/19.
+//  Copyright © 2019 Kyle Braden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
